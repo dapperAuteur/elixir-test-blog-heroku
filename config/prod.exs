48add7875d5b714061c054bdc201fbfca8add2e4
Anthony McDonald <a@awews.com>
2017-10-28 12:16:14 -0600

    add comma at end of line 21

--- config/prod.exs
+++ config/prod.exs
@@ -18,7 +18,7 @@ config :teacher, Teacher.Endpoint,
   cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
-config :teacher, Teacher.Repo
+config :teacher, Teacher.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
